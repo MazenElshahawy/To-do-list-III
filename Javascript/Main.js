efff3bf833bc6b8da508b39be5b679ba170894cc
Mazen <Mazen.El-Shahawy@outlook.com>
2022-12-03 10:58:59 +0200

    Daily Edit

--- Javascript/Main.js
+++ Javascript/Main.js
@@ -1,8 +1,10 @@
+
+
 window.addEventListener("load", () => {
   const form = document.querySelector("#new-task-form")
   const input = document.querySelector("#new-task-input")
   const list_el = document.querySelector("#tasks")
-  const buttons = document.querySelector(".actions")
+
 
   form.addEventListener("submit", (e) => {
     e.preventDefault();
@@ -20,9 +22,9 @@ window.addEventListener("load", () => {
 
     const task_input_el = document.createElement("input");
     task_input_el.classList.add("text");
-    task_input_el.type ="text"
+    task_input_el.type = "text"
     task_input_el.value = task;
-    task_input_el.setAttribute("readonly","readonly")
+    task_input_el.setAttribute("readonly", "readonly")
 
     const task_actions_el = document.createElement("div")
     task_actions_el.classList.add("actions")
@@ -48,14 +50,18 @@ window.addEventListener("load", () => {
 
 
     task_edit_el.addEventListener("click", () => {
-      if (task_edit_el.innerText.toLocaleLowerCase() =="edit" ) {
+      if (task_edit_el.innerText.toLocaleLowerCase() == "edit") {
         task_input_el.removeAttribute("readonly");
         task_input_el.focus();
         task_edit_el.innerText = "Save";
-      } else{
+      } else {
         task_input_el.setAttribute("readonly", "readonly");
         task_edit_el.innerText = "edit";
       }
+
+      task_delete_el.addEventListener("click", () => {
+        list_el.removeChild(task_el);
+      })
     })
   })
 
